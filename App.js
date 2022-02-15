ca60ee34adf38e8680008292653f6e36a6485fa7
Ibby <ibby364@outlook.com>
2022-02-15 18:19:13 +0000

    Change to functional component

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { Component } from 'react'
+import React from 'react'
 import Landing from './components/screens/Landing';
+import Login from './components/screens/Login';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
-class App extends Component {
-
-    render(){
+const App = () => {
         return (
             <NavigationContainer>
                 <Stack.Navigator>
                     <Stack.Screen name="Landing" component={Landing} options={{headerShown: false}}/>
-                    {/*<Stack.Screen name="Signup" component={Signup}/>
                     <Stack.Screen name="Login" component={Login}/>
+                    {/*<Stack.Screen name="Login" component={Login}/>
                     <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>*/}
                 </Stack.Navigator>
             </NavigationContainer>
             );
-        }
-
-  }
-
-
-
-
+    }
 export default App;
