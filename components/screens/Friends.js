90ef60fbf1b50d2b049249520f164df40e9daa22
Ibby <ibby364@outlook.com>
2022-02-15 18:39:07 +0000

    Added Tab Screens

---
+++ components/screens/Friends.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View,} from 'react-native';
+
+class Friends extends Component {
+    render(){
+        return (
+            <View></View>
+        )
+    }
+}
+
+export default Friends;
---
+++ components/screens/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View,} from 'react-native';
+
+class Search extends Component {
+    render(){
+        return(
+            <View></View>
+        )
+    }
+}
+
+export default Search;
