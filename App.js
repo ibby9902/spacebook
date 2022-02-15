258880513917112ba6ee302e389bfe730d4ee997
Ibby <ibby364@outlook.com>
2022-02-15 18:38:27 +0000

    Added Signup & Main to Stack

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react'
 import Landing from './components/screens/Landing';
 import Login from './components/screens/Login';
+import Signup from './components/screens/Signup'
+import Main from './components/Main'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
@@ -13,8 +15,8 @@ const App = () => {
                 <Stack.Navigator>
                     <Stack.Screen name="Landing" component={Landing} options={{headerShown: false}}/>
                     <Stack.Screen name="Login" component={Login}/>
-                    {/*<Stack.Screen name="Login" component={Login}/>
-                    <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>*/}
+                    <Stack.Screen name="Signup" component={Signup}/>
+                    <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>
                 </Stack.Navigator>
             </NavigationContainer>
             );
