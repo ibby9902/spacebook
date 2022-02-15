e718107a756e3d4019fed5b0a099dd7564aa5845
Ibby <ibby364@outlook.com>
2022-02-15 18:55:46 +0000

    onLoginPress implemented

--- components/screens/Login.js
+++ components/screens/Login.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, Button, TextInput } from 'react-native';
-import storeData from "../../functions/storeData";
+import storeData from '../../functions/storeData';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}) => {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
 
 
-    const login = () => {
-        console.log("login pressed")
+    const onLoginPress = () => {
+        let to_send = {
+            email: email,
+            password: password
+        };
+    
+        return fetch("http://localhost:3333/api/1.0.0/login" , {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(to_send)
+            })
+            .then((response) => {
+                response.json().then(data => {
+                    storeData("id",data.id)
+                    storeData("token",data.token)
+                })
+                navigation.navigate("Main")
+            })
     }
 
     return(
         <View style={styles.container}>
             <TextInput placeholder="Email" onChangeText={(val) => setEmail(val)}/>
             <TextInput placeholder="Password"secureTextEntry={true} onChangeText={(val) => setPassword(val)}/>
-            <TouchableOpacity title="Login"style={styles.loginButton}  onPress={login}><Text>Login</Text></TouchableOpacity>
+            <TouchableOpacity title="Login"style={styles.loginButton}  onPress={onLoginPress}><Text>Login</Text></TouchableOpacity>
         </View>
     )
 }
