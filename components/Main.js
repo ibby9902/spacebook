a359efeca269be9f180c7b7b328b587dfbe85497
Ibby <ibby364@outlook.com>
2022-02-15 20:42:08 +0000

    Changed to functional component

--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Profile from './screens/Profile'
 import Friends from './screens/Friends'
@@ -8,17 +8,14 @@ const Tab = createBottomTabNavigator();
 
 
 
-class Main extends Component {
-    render(){
-        return (
-            <Tab.Navigator>
-                <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
-                <Tab.Screen name="Search" component={Search} options={{headerShown: false}}/>
-                <Tab.Screen name="Friends" component={Friends} options={{headerShown: false}}/>
-            </Tab.Navigator>
-        )
-
-    }
+const Main = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
+            <Tab.Screen name="Search" component={Search} options={{headerShown: false}}/>
+            <Tab.Screen name="Friends" component={Friends} options={{headerShown: false}}/>
+        </Tab.Navigator>
+    )
 }
 
 export default Main;
