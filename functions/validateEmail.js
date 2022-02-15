0c934a79795d51817b4a6b62a2e9b51de7127d38
Ibby <ibby364@outlook.com>
2022-02-15 20:20:15 +0000

    Validate Email function

---
+++ functions/validateEmail.js
@@ -0,0 +1,7 @@
+const validateEmail = (email, setValidEmail) => {
+    const regex = /\S+@\S+\.\S+/;
+    const isValid = regex.test(email);
+    setValidEmail(isValid);
+}
+
+export default validateEmail;
