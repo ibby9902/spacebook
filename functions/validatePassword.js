626d1ce7ccb6778fb22ec1bc194dcd35528d7a0a
Ibby <ibby364@outlook.com>
2022-02-15 20:20:26 +0000

    Validate Password function

---
+++ functions/validatePassword.js
@@ -0,0 +1,11 @@
+// Passwords must be have a length greater than 5 characters
+const validatePassword = (password, setValidPassword) =>{
+    if(password.length > 5){
+        setValidPassword(true);
+    }
+    else {
+        setValidPassword(false)
+    }
+}
+
+export default validatePassword;
