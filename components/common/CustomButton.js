ed5a54d18ae00c4972c88ef9de816e1d268293bb
Ibby <ibby364@outlook.com>
2022-02-16 17:55:50 +0000

    Added CustomButton

---
+++ components/common/CustomButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const CustomButton = ({text, onClick, style, textStyle}) => {
+    return (
+        <TouchableOpacity style={style} onPress={onClick}>
+            <View style={styles.textContainer}>
+                <Text style={textStyle}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+    textContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+})
+export default CustomButton; 
