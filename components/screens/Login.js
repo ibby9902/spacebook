2c74c9c1ad1229bd32e5a0572fc918272aee3472
Ibby <ibby364@outlook.com>
2022-02-16 17:56:56 +0000

    Fixed fetch request promises & added custom button

--- components/screens/Login.js
+++ components/screens/Login.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, Button, TextInput } from 'react-native';
 import storeData from '../../functions/storeData';
-
+import CustomInput from '../common/CustomInput';
+import CustomButton from '../common/CustomButton';
 const LoginScreen = ({navigation}) => {
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
 
     const onLoginPress = () => {
@@ -21,19 +22,32 @@ const LoginScreen = ({navigation}) => {
                 body: JSON.stringify(to_send)
             })
             .then((response) => {
-                response.json().then(data => {
-                    storeData("id",data.id)
-                    storeData("token",data.token)
-                })
-                navigation.navigate("Main")
+                if(response.status === 200){
+                    return response.json()
+                }
+                else if(response.status === 400) {
+                    throw "Invalid email or passord"
+                }
+                else {
+                    throw "Something went wrong"
+                }
+            })
+            .then(async (responseJson) => {
+                await storeData("id",responseJson.id)
+                await storeData("token",responseJson.token)
+                navigation.navigate("Main");
+            })
+            .catch((error) => {
+                console.log(error);
             })
     }
 
     return(
         <View style={styles.container}>
-            <TextInput placeholder="Email" onChangeText={(val) => setEmail(val)}/>
-            <TextInput placeholder="Password"secureTextEntry={true} onChangeText={(val) => setPassword(val)}/>
-            <TouchableOpacity title="Login"style={styles.loginButton}  onPress={onLoginPress}><Text>Login</Text></TouchableOpacity>
+            <CustomInput placeholder="Email" setValue={setEmail} value={email}/>
+            <CustomInput placeholder="Password" secureEntry={true} setValue={setPassword} value={password}/>
+            <CustomButton text="Login"  onClick={onLoginPress} style={styles.loginButton} textStyle={styles.loginText}/>
+            <CustomButton text="Sign Up "  onClick={() => navigation.navigate("Signup")} style={styles.signupButton} textStyle={styles.signupText}/>
         </View>
     )
 }
@@ -45,8 +59,28 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     loginButton: {
-        backgroundColor: 'red',
+        width: '75%',
+        height: 50,
+        padding: 15,
+        marginVertical: 5,
+        borderRadius: 5,
+        backgroundColor: 'dodgerblue'
+    },
+    signupButton: {
+        width: '75%',
+        height: 50,
+        padding: 15,
+        marginVertical: 5,
+        borderRadius: 5,
         
+    },
+    signupText: {
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    loginText: {
+        fontWeight: 'bold',
+        color: 'white'
     }
 })
 export default LoginScreen;
