4ca0c2e3bcc7becffb0d5f75f5e9182d23132fc2
Ibby <ibby364@outlook.com>
2022-02-16 12:40:17 +0000

    Change to class component & add focus listener

--- components/Main.js
+++ components/Main.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Profile from './screens/Profile'
 import Friends from './screens/Friends'
 import Search from './screens/Search'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import getToken from '../functions/getToken'
 const Tab = createBottomTabNavigator();
 
 
 
 
-const Main = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
-            <Tab.Screen name="Search" component={Search} options={{headerShown: false}}/>
-            <Tab.Screen name="Friends" component={Friends} options={{headerShown: false}}/>
-        </Tab.Navigator>
-    )
+class Main extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        this.unsubscribe = this.props.navigation.addListener('focus', () => {
+            this.checkLoggedIn();
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    checkLoggedIn = async () => {
+        const value = await getToken();
+        console.log(value)
+        if(value == null)
+            this.props.navigation.navigate("Landing")
+        
+    }
+    render(){
+        return (
+            <Tab.Navigator>
+                <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
+                <Tab.Screen name="Search" component={Search} options={{headerShown: false}}/>
+                <Tab.Screen name="Friends" component={Friends} options={{headerShown: false}}/>
+            </Tab.Navigator>
+        )
+    }
+    
 }
 
 export default Main;
