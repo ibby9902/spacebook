cd98eb3a53b8944176a7b2a448061d0eab91b950
Ibby <ibby364@outlook.com>
2022-02-21 18:24:13 +0000

    Added Accept & Reject functions

--- components/common/FriendRequest.js
+++ components/common/FriendRequest.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import getToken from '../../functions/getToken';
 import CustomButton from './CustomButton';
 
 class FriendRequest extends Component{
     constructor(props){
         super(props)
     }
+    
+    acceptFriendRequest = async () => {
+        const token = await getToken();
+        return fetch(`http://localhost:3333/api/1.0.0/friendrequests/${this.props.id}`, {
+            headers: {
+                "X-AUTHORIZATION": token
+            },
+            method: 'post'
+        })
+        .then(this.removeRequest)
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    rejectFriendRequest = async () => {
+        const token = await getToken();
+        return fetch(`http://localhost:3333/api/1.0.0/friendrequests/${this.props.id}`, {
+            headers: {
+                "X-AUTHORIZATION": token
+            },
+            method: 'delete'
+        })
+        .then(this.removeRequest)
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    removeRequest = () => {
+        const i = this.props.data((user, index) => {
+            if(user.user_id === this.props.id)
+                return true;
+        })
+        if(i > -1)
+            this.props.data.splice(i,1);
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -13,8 +52,8 @@ class FriendRequest extends Component{
                     <Text>{this.props.firstName} <Text>{this.props.lastName}</Text></Text>
                 </View>
                 <View style={styles.buttonContainer}>
-                    <CustomButton text='Accept' style={styles.accept} />
-                    <CustomButton text='Reject' style={styles.accept} />
+                    <CustomButton text='Accept' style={styles.accept} onClick={this.acceptFriendRequest} />
+                    <CustomButton text='Reject' style={styles.reject} onClick={this.rejectFriendRequest}/>
 
                 </View>
                 
@@ -49,6 +88,12 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-evenly',
         alignItems: 'center'
+    },
+    reject: {
+        width: '25%',
+        height: 30,
+        borderRadius: 5,
+        backgroundColor: 'red'
     }
 })
 
