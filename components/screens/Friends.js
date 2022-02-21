55f9a1f61aa5f29a600429d88954db529cbb54d6
Ibby <ibby364@outlook.com>
2022-02-21 18:26:04 +0000

    Added FlatList & get data

--- components/screens/Friends.js
+++ components/screens/Friends.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { View, ScrollView, StyleSheet} from 'react-native';
+import { View, ScrollView, StyleSheet, FlatList} from 'react-native';
+import getToken from '../../functions/getToken';
+import getId from '../../functions/getId'
 import CustomButton from '../common/CustomButton';
 import Friend from '../common/Friend';
 class Friends extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            isLoading: true,
+            friendData: []
+        }
         
     }
 
-    
+    getFriendData = async (id) => {
+        const token =  await getToken();
+        return fetch(`http://localhost:3333/api/1.0.0/user/${id}/friends`,{
+            headers: {
+                "X-AUTHORIZATION": token
+            },
+            method: 'get'
+        })
+        .then((response) => {
+            if(response.status === 200)
+                return response.json();
+        })
+        .then((responseJson) => {
+            this.setState({
+                isLoading: false,
+                friendData: responseJson})
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    componentDidMount() {
+        getId().then((id) => this.getFriendData(id))
+    }
 
     render(){
         return (
@@ -16,8 +46,8 @@ class Friends extends Component {
                 <View style={styles.header}>
                     <CustomButton text="Friend Requests" style={styles.button} onClick={() => this.props.navigation.navigate("Friend Requests")} />
                 </View>
-                <Friend/>
-                {/* List of Friend Components */}
+                <FlatList data={this.state.friendData} renderItem={({item}) => <Friend firstName={item.user_givenname} lastName={item.user_familyname} id={item.user_id} state_data={this.state.friendData}/>}
+                keyExtractor={({user_id}, index) => user_id}/>
             </ScrollView>
         )
     }
