c15b9b9622303d58798f0db214fbc9a57f19b309
Ibby <ibby364@outlook.com>
2022-02-21 18:25:28 +0000

    Declared delete & view profile functions

--- components/common/Friend.js
+++ components/common/Friend.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import CustomButton from './CustomButton';
 
 class Friend extends Component {
+
+
+    deleteFriend = () => {
+        console.log("friend deleted")
+    }
+
+    viewProfile = () => {
+        //this.props.navigation.navigate("Profile")
+    }
+
     render(){
         return (
             <View style={styles.container}>
-                <Text>Friend</Text>
+                <Text>{this.props.firstName} <Text>{this.props.lastName}</Text></Text>
+                <View style={styles.buttonContainer}>
+                    <CustomButton text='View Profile' style={styles.deleteButton} onClick={this.viewProfile}/>
+                    <CustomButton text='Delete' style={styles.deleteButton} onClick={this.deleteFriend}/>
+                </View>
             </View>
         )
     }
@@ -17,6 +32,18 @@ const styles = StyleSheet.create({
         height: 100,
         backgroundColor: 'red',
         justifyContent: 'center',
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    deleteButton: {
+        backgroundColor: 'orange'
+    },
+    viewProfile: {
+        backgroundColor: 'blue'
     }
 })
 
