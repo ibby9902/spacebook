c999b9eb0283c99d493b599dd564aab20234beb7
Ibby <ibby364@outlook.com>
2022-02-24 01:25:34 +0000

    Added tab bar icons

--- components/Main.js
+++ components/Main.js
@@ -4,6 +4,7 @@ import Profile from './screens/Profile'
 import FriendsStack from './navigation/FriendsStack'
 import Search from './screens/Search'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import getToken from '../functions/getToken'
 const Tab = createBottomTabNavigator();
 
@@ -33,7 +34,21 @@ class Main extends Component {
     }
     render(){
         return (
-            <Tab.Navigator>
+            <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarLabel: '',
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+                    if(route.name === 'Profile'){
+                        iconName = 'person-circle-outline'
+                    } else if(route.name === 'Search'){
+                        iconName = 'search-outline'
+                    } else if(route.name === 'Friends'){
+                        iconName = 'people-outline'
+                    }
+                    return <Ionicons name={iconName} size={size} color={color}/>
+                }
+            })}>
                 <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}}/>
                 <Tab.Screen name="Search" component={Search} options={{headerShown: false}}/>
                 <Tab.Screen name="Friends" component={FriendsStack} options={{headerShown: false}}/>
