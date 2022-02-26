412331a7224d986f8cf1654175c1eea7c8fce552
Ibby <ibby364@outlook.com>
2022-02-26 03:57:08 +0000

    Updated styles

--- components/common/CustomInput.js
+++ components/common/CustomInput.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import theme from '../../assets/theme'
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const CustomInput = ({value, setValue, placeholder, secureEntry, style}) => {
     return(
         <View style={styles.container}>
-            <TextInput placeholder={placeholder} style={style} value={value} onChangeText={setValue} secureTextEntry={secureEntry}/>
+            <TextInput placeholder={placeholder} style={{backgroundColor: theme.DARK_GREY, color: 'white'}} value={value} onChangeText={setValue} secureTextEntry={secureEntry}/>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
+        backgroundColor: theme.DARK_GREY,
         width: '75%',
         height: 40,
         borderColor: '#e8e8e8',
