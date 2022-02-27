cd283a78fd20bab77507e84ff0d4f04e038ec3e0
Ibby <ibby364@outlook.com>
2022-02-27 19:08:55 +0000

    Change to functional, abstract rejcct & accept

--- components/common/FriendRequest.js
+++ components/common/FriendRequest.js
@@ -3,75 +3,43 @@ import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import getToken from '../../functions/getToken';
 import CustomButton from './CustomButton';
 import theme from '../../assets/theme';
-
-class FriendRequest extends Component{
-    constructor(props){
-        super(props)
-    }
+import acceptFriend from '../../functions/requests/acceptFriend';
+import rejectFriend from '../../functions/requests/rejectFriend';
+import removeFriendRequest from '../../functions/removeFriendRequest'
+const FriendRequest = (props) => {
     
-    acceptFriendRequest = async () => {
-        const token = await getToken();
-        return fetch(`http://localhost:3333/api/1.0.0/friendrequests/${this.props.id}`, {
-            headers: {
-                "X-AUTHORIZATION": token
-            },
-            method: 'post'
-        })
-        .then(this.removeRequest)
-        .catch((error) => {
-            console.log(error)
-        })
+    const handleAccept = () => {
+        acceptFriend(props.id)
+        removeFriendRequest(props.data, props.id)
+    }
+    const handleReject = () => {
+        rejectFriend(props.id)
+        removeFriendRequest(props.data, props.id)
     }
 
-    rejectFriendRequest = async () => {
-        const token = await getToken();
-        return fetch(`http://localhost:3333/api/1.0.0/friendrequests/${this.props.id}`, {
-            headers: {
-                "X-AUTHORIZATION": token
-            },
-            method: 'delete'
-        })
-        .then(this.removeRequest)
-        .catch((error) => {
-            console.log(error)
-        })
-    }
-
-    removeRequest = () => {
-        const i = this.props.data((user, index) => {
-            if(user.user_id === this.props.id)
-                return true;
-        })
-        if(i > -1)
-            this.props.data.splice(i,1);
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.nameText}>{this.props.firstName} <Text style={styles.nameText}>{this.props.lastName}</Text></Text>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <CustomButton text='Accept' style={styles.accept} onClick={this.acceptFriendRequest} textStyle={{color: theme.TEXT_WHITE, fontWeight: 'bold'}}/>
-                    <CustomButton text='Reject' style={styles.reject} onClick={this.rejectFriendRequest} textStyle={{color: theme.TEXT_WHITE, fontWeight: 'bold'}}/>
-
-                </View>   
+    return (
+        <View style={styles.container}>
+            <View style={styles.textContainer}>
+                <Text style={styles.nameText}>{props.firstName} <Text style={styles.nameText}>{props.lastName}</Text></Text>
             </View>
-        )
+            <View style={styles.buttonContainer}>
+                <CustomButton text='Accept' style={styles.accept} onClick={handleAccept} textStyle={{color: theme.TEXT_WHITE, fontWeight: 'bold'}}/>
+                <CustomButton text='Reject' style={styles.reject} onClick={handleReject} textStyle={{color: theme.TEXT_WHITE, fontWeight: 'bold'}}/>
 
-    }
+            </View>   
+        </View>
+    )    
 }   
 
 const styles = StyleSheet.create({
     container: {
         height: 100,
-        width: '80%',
+        width: '100%',
         backgroundColor: theme.GREY_BLUE,
         flexDirection: 'row',
     },
     accept: {
-        width: '25%',
+        width: '35%',
         height: 30,
         borderRadius: 5,
         backgroundColor: theme.YELLOW
@@ -88,7 +56,7 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     reject: {
-        width: '25%',
+        width: '35%',
         height: 30,
         borderRadius: 5,
         backgroundColor: theme.BUTTON_DARK_BLUE
