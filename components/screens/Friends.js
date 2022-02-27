8053c155ef76ca4c3fe87a134e470b59289bdfa7
Ibby <ibby364@outlook.com>
2022-02-27 19:07:53 +0000

    Change to functional,, abstracted get friends

--- components/screens/Friends.js
+++ components/screens/Friends.js
@@ -1,72 +1,71 @@
-import React, { Component } from 'react';
-import { View, ScrollView, StyleSheet, FlatList} from 'react-native';
-import getToken from '../../functions/getToken';
-import getId from '../../functions/getId'
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, StyleSheet, FlatList, ActivityIndicator, Text} from 'react-native';
 import CustomButton from '../common/CustomButton';
 import Friend from '../common/Friend';
-class Friends extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            isLoading: true,
-            friendData: []
-        }
-        
-    }
+import getFriends from '../../functions/requests/getFriends';
+import theme from '../../assets/theme';
+const Friends = (props) => {
 
-    getFriendData = async (id) => {
-        const token =  await getToken();
-        return fetch(`http://localhost:3333/api/1.0.0/user/${id}/friends`,{
-            headers: {
-                "X-AUTHORIZATION": token
-            },
-            method: 'get'
-        })
-        .then((response) => {
-            if(response.status === 200)
-                return response.json();
-        })
-        .then((responseJson) => {
-            this.setState({
-                isLoading: false,
-                friendData: responseJson})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
+    const [isLoading, setIsLoading] = useState(true);
+    const [friends, setFriends] = useState([]);
 
-    componentDidMount() {
-        getId().then((id) => this.getFriendData(id))
-    }
+    useEffect(() => {
+        getFriends(props.route.params.id, setIsLoading, setFriends);
+    }, [])
 
-    render(){
+    if(isLoading)
+    {
         return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.DARK_GREY}}>
+            <ActivityIndicator color={theme.TEXT_WHITE}/>
+            </View>
+        )
+    }
+    else 
+    {
+        return (
+        
             <ScrollView style={styles.container}>
                 <View style={styles.header}>
-                    <CustomButton text="Friend Requests" style={styles.button} onClick={() => this.props.navigation.navigate("Friend Requests")} />
+                    <View style={styles.contentContainer}>
+                        <Text style={{color: theme.TEXT_WHITE, fontWeight: 'bold', fontSize: 25}}>Friends</Text>
+                    <CustomButton text="Requests" textStyle={styles.btnText} style={styles.button} onClick={() => props.navigation.navigate("Friend Requests")} />
+                    </View>
                 </View>
-                <FlatList data={this.state.friendData} renderItem={({item}) => <Friend firstName={item.user_givenname} lastName={item.user_familyname} id={item.user_id} state_data={this.state.friendData}/>}
+                <FlatList data={friends} renderItem={({item}) => <Friend firstName={item.user_givenname} lastName={item.user_familyname} id={item.user_id} state_data={friends}/>}
                 keyExtractor={({user_id}, index) => user_id}/>
             </ScrollView>
         )
     }
+     
+        
+    
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#eaedf4'
+        backgroundColor: theme.DARK_GREY
     },
     header: {
-        height: 200,
-        backgroundColor: 'blue',
-        justifyContent: 'center',
-        alignItems: 'center'
-
+        height: 150,
+    },
+    contentContainer: {
+        marginTop: 50,
+        marginHorizontal: 15,
+        borderBottomWidth: 1,
+        borderColor: theme.TEXT_LESS_WHITE,
+        paddingBottom: 10
     },
     button: {
-        width: '20%',
-        backgroundColor: 'red'
+        marginTop: 10,
+        width: '25%',
+        height: 30,
+        backgroundColor: theme.BUTTON_DARK_BLUE,
+        borderRadius: 25,
+    },
+    btnText: {
+        color: theme.TEXT_WHITE,
+        fontWeight: 'bold'
     }
 })
 
