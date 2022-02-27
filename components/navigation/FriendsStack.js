a1543c4fadb9b127395e4f52976ce79a2d48db19
Ibby <ibby364@outlook.com>
2022-02-27 23:17:31 +0000

    Add Profile stack screen,

--- components/navigation/FriendsStack.js
+++ components/navigation/FriendsStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Friends from '../screens/Friends';
 import FriendRequests from '../screens/FriendRequests';
+import Profile from '../screens/Profile'
 const Stack = createNativeStackNavigator();
 
 const FriendsStack = ({route}) => {
@@ -9,6 +10,7 @@ const FriendsStack = ({route}) => {
         <Stack.Navigator>
             <Stack.Screen name="FriendsScreen" component={Friends} options={{headerShown: false}} initialParams={{id: route.params.id}}/>
             <Stack.Screen name="Friend Requests" component={FriendRequests} />
+            <Stack.Screen name="Profile" component={Profile}/>
         </Stack.Navigator>
     )
 }
