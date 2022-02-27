63ed35bb3bff9f4c02e839fe426e6410243615c0
Ibby <ibby364@outlook.com>
2022-02-27 19:17:41 +0000

    Changed to functional component

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { Component } from 'react'
-import Landing from './components/screens/Landing';
 import Login from './components/screens/Login';
-import Signup from './components/screens/Signup'
-import Main from './components/Main'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import Signup from './components/screens/Signup';
+import Main from './components/Main';
 
 const Stack = createNativeStackNavigator();
 
-class App extends Component {
-        render() {
-            return (
-                <NavigationContainer>
-                    <Stack.Navigator initialRouteName='Login'>
-                        <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>
-                        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-                        <Stack.Screen name="Signup" component={Signup}/>
-                    </Stack.Navigator>
-                </NavigationContainer>
-                );
-        }
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Login'>
+                <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>
+                <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+                <Stack.Screen name="Signup" component={Signup}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+        );
 }
 export default App;
