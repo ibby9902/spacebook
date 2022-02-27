349c717a5f2e707f121a1915be80fbec0547dc9c
Ibby <ibby364@outlook.com>
2022-02-27 19:09:17 +0000

    Change to functional, pass id to Friends

--- components/navigation/FriendsStack.js
+++ components/navigation/FriendsStack.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';  
+import React from 'react';  
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Friends from '../screens/Friends';
 import FriendRequests from '../screens/FriendRequests';
 const Stack = createNativeStackNavigator();
 
-class FriendsStack extends Component {
-    render() {
-        return (
-            <Stack.Navigator>
-                <Stack.Screen name="FriendsScreen" component={Friends} options={{headerShown: false}}/>
-                <Stack.Screen name="Friend Requests" component={FriendRequests}/>
-            </Stack.Navigator>
-        )
-    }
+const FriendsStack = ({route}) => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="FriendsScreen" component={Friends} options={{headerShown: false}} initialParams={{id: route.params.id}}/>
+            <Stack.Screen name="Friend Requests" component={FriendRequests} />
+        </Stack.Navigator>
+    )
 }
 
-export default FriendsStack
+export default FriendsStack;
