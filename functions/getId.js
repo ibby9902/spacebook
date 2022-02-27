0ffa915ffac0e74c9d103ae5392bdc7d00bb13c3
Ibby <ibby364@outlook.com>
2022-02-27 06:07:39 +0000

    Null check

--- functions/getId.js
+++ functions/getId.js
@@ -4,7 +4,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const getId = async () => {
     try {
       const value = await AsyncStorage.getItem('@id')
-      return value
+      if(value !== null)
+        return value
     } catch(e) {
       // error reading value
     }
