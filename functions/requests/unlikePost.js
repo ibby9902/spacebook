9503d775b357c75cfb7c09a64b6b133e4defeba1
Ibby <ibby364@outlook.com>
2022-02-27 06:08:31 +0000

    initial unlike request function

---
+++ functions/requests/unlikePost.js
@@ -0,0 +1,21 @@
+import getToken from '../../functions/getToken';
+
+const unlikePost = async (user_id, post_id) => {
+     // send DELETE like request to server
+     const token = await getToken();
+     return fetch(`http://localhost:3333/api/1.0.0/user/${user_id}/post/${post_id}/like`, {
+        headers: {
+            "X-AUTHORIZATION": token
+        },
+        method: 'delete'
+    })
+    .then((response) => {
+        if(response.status === 200) {
+            console.log("post unliked")
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
+export default unlikePost;
