3abad439ee0bf3f0efeb73ea6257ac5f89406c73
Ibby <ibby364@outlook.com>
2022-02-27 06:05:52 +0000

    Changed to functional component

--- components/common/Post.js
+++ components/common/Post.js
@@ -1,34 +1,76 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-class Post extends Component {
-    constructor(props){
-        super(props)
-    }
-    render() {
-        return (
-            <View style={styles.postContainer}>
-                <Text style={styles.textColor}>{this.props.data.author.first_name}<Text style={styles.textColor}>{this.props.data.author.last_name}</Text></Text>
-                <Text style={styles.textColor}>{this.props.data.text}</Text>
-                <Text style={styles.textColor}>Likes: {this.props.data.numLikes}</Text>
-            </View>
-        )
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import getId from '../../functions/getId';
+import likePost from '../../functions/requests/likePost';
+import unlikePost from '../../functions/requests/unlikePost';
+const Post = (props) => {
 
+    const [like, setLike] = useState(false);
+    const [myID, setMyID] = useState(0)
+
+    useEffect(() => {
+        getId().then((res) => setMyID(parseInt(res)))
+    },[]) 
+
+    const handleLikePost = async () => {
+        const likeStatus = !like;
+        setLike(likeStatus)
+        if(likeStatus){
+            likePost(props.data.author.user_id, props.data.post_id)   
+        }
+        else {
+            unlikePost(props.data.author.user_id, props.data.post_id)
+        }
     }
+
+    const renderLikeButton = () => {
+        if(myID !== props.data.author.user_id)
+        {
+            return (
+                <TouchableOpacity onPress={handleLikePost} style={{width: 48, height: 48}}>
+                    <Ionicons name={like ? 'heart': 'heart-outline'} size={48} color={like ? 'red' : 'white'}/>
+                </TouchableOpacity>
+            )
+        }
+    }
+
+    return (
+        <View style={styles.postContainer}>
+        <TouchableOpacity onPress={()=> console.log(`Open post ${props.data.post_id}`)}>
+        <View >
+            <Text style={styles.titleText}>{props.data.author.first_name} <Text style={styles.titleText}>{props.data.author.last_name}</Text></Text>
+            <Text style={styles.postText}>{props.data.text}</Text>
+            <Text style={styles.numLikesText}>Likes: {props.data.numLikes}</Text>
+            
+        </View>
+
+        </TouchableOpacity>
+        {renderLikeButton()}
+        </View>
+    )
+
+    
 }
 
 const styles = StyleSheet.create({
     postContainer: {
-        height: 100,
+        height: 150,
         width: '100%',
-        backgroundColor: '#191a2c',
+        backgroundColor: '#181c25',
         marginVertical: 10,
         paddingHorizontal: 10,
-        borderWidth: 2,
-        borderColor: '#32344c',
-        borderRadius: 5
     },
-    textColor: {
+    titleText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    postText: {
         color: 'white'
+    },
+    numLikesText:{
+        color: 'white',
+        fontWeight: 'bold'
     }
 });
 
