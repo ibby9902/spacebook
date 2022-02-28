d69f9fb62be30acde6b5166d1384224fc977bfef
Ibby <ibby364@outlook.com>
2022-02-28 23:28:29 +0000

    Change styles, added icon

--- components/common/Friend.js
+++ components/common/Friend.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import CustomButton from './CustomButton';
-import theme from '../../assets/theme'
+import theme from '../../assets/theme';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Friend = (props) => {
     return (
         <View style={styles.container}>
-            <View style={styles.nameContainer}>
-                <Text style={styles.nameText}>{props.firstName} <Text style={styles.nameText}>{props.lastName}</Text></Text>
+            <View style={styles.icon}>
+                <Ionicons name={'person-outline'} size={70} color={theme.TEXT_WHITE}/>
             </View>
-            <View style={styles.buttonContainer}>
+            <View style={styles.content}>
+                <Text style={styles.nameText}>{props.firstName} <Text style={styles.nameText}>{props.lastName}</Text></Text>
                 <CustomButton text='View Profile' style={styles.viewProfile} onClick={() => props.viewProfile(props.id)} textStyle={styles.btnText}/>
             </View>
         </View>
@@ -20,19 +22,15 @@ const Friend = (props) => {
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height: 100,
+        height: 90,
         backgroundColor: theme.GREY_BLUE,
+        flexDirection: 'row',
         justifyContent: 'center',
     },
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-    },
     viewProfile: {
-        width: '40%',
-        height: 60,
-        backgroundColor: theme.BUTTON_DARK_BLUE
+        width: 100,
+        height: 30,
+        backgroundColor: theme.YELLOW,
     },
     btnText: {
         color: theme.TEXT_WHITE,
@@ -41,10 +39,15 @@ const styles = StyleSheet.create({
     nameText: {
         color: theme.TEXT_WHITE,
         fontWeight: 'bold',
-        fontSize: 20,
+        fontSize: 15,
     },
-    nameContainer: {
+    content: {
+        flex: 3,
+        justifyContent: 'space-around'
+    },
+    icon: {
         flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     }
 })
