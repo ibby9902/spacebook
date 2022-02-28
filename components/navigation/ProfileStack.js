b837023f53290fb3f070462a13d3bd090d986ad3
Ibby <ibby364@outlook.com>
2022-02-28 23:29:39 +0000

    Added UsersFriends screen to stack

--- components/navigation/ProfileStack.js
+++ components/navigation/ProfileStack.js
@@ -1,12 +1,14 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from '../screens/Profile'
+import UsersFriends from '../screens/UsersFriends';
 const Stack = createNativeStackNavigator();
 
 const ProfileStack = ({route}) => {
     return (
         <Stack.Navigator initialRouteName='Profile'>
                         <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}} initialParams={{id: route.params.id}}/>
-                        {/* <Stack.Screen name="FriendsList" component={Login} options={{headerShown: false}}/> a whole screen with a list of friends? */}
+                        {/*a whole screen with a list of friends? */}
+                        <Stack.Screen name="UsersFriends" component={UsersFriends} initialParams={{id: route.params.id}}/>
                         {/* <Stack.Screen name="SinglePost" component={Signup}/> A whole screen for a single post??*/}
                         {/* Profile stack screen here? So we can click friends on the friends list to show their profile? */} 
         </Stack.Navigator>
