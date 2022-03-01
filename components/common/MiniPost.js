da31000caf919a5fd6256b179208a79e37a072ce
Ibby <ibby364@outlook.com>
2022-03-01 01:04:54 +0000

    Added full name, date, like counter

--- components/common/MiniPost.js
+++ components/common/MiniPost.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import theme from '../../assets/theme';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const MiniPost = () => {
+const MiniPost = (props) => {
     return (
         <TouchableOpacity onPress={() => console.log("Minipost clicked")} style={styles.container}>
-            <View >
-                <Text style={{fontWeight: 'bold', color: theme.TEXT_WHITE}}>FirstName LastName</Text>
-                <Text style={{fontWeight: 'bold', color: theme.TEXT_WHITE}}>26/02/2022</Text>
+            <View style={{flex: 1, alignItems: 'center'}}>
+            <View style={{ width: '90%', borderBottomColor: theme.TEXT_LESS_WHITE, borderBottomWidth: 1}}>
+                <Text style={styles.nameText}>{props.data.author.first_name} <Text style={styles.nameText}>{props.data.author.last_name}</Text></Text>
+                <Text style={styles.data}>{props.data.timestamp}</Text>
+            </View>
+            <View style={{width: '90%', flexDirection: 'row-reverse'}}>
+                <Text style={styles.likeText}>Likes: {props.data.numLikes}</Text>
+            </View>
+
             </View>
         </TouchableOpacity>
 
@@ -16,11 +23,19 @@ const MiniPost = () => {
 
 const styles = StyleSheet.create({
     container: {
-        width: '80%',
-        height: 80,
+        height: 60,
         backgroundColor: theme.LIGHT_GRAY,
         marginVertical: 10,
-        borderRadius: 10,
+    },
+    nameText: {
+        fontWeight: 'bold', 
+        color: theme.TEXT_WHITE
+    },
+    data: {
+        color: theme.TEXT_LESS_WHITE
+    },
+    likeText:{
+        color: theme.TEXT_LESS_WHITE
     }
 })
 
