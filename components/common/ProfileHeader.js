dbf9a84dbf36fd5746614b5a15a60a998f984d58
Ibby <ibby364@outlook.com>
2022-03-01 01:05:38 +0000

    Changed styling

--- components/common/ProfileHeader.js
+++ components/common/ProfileHeader.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity} from 'react-native'
-
+import CustomButton from './CustomButton';
+import theme from '../../assets/theme';
 const ProfileHeader = (props) => {
+
     return(
         <View style={styles.profileHeader}>
             <TouchableOpacity style={styles.imageContainer}/>
-            
+            <View style={{flexDirection:'row', marginVertical: 10}}>
+                <View style={{flex: 1, justifyContent: 'center', paddingRight: 10}}>
                     <Text style={styles.name}>{props.firstName} <Text style={styles.name}>{props.lastName}</Text></Text>
+                </View>
+                <CustomButton text={`Friends: ${props.friendCount}`} style={styles.friendsButton} textStyle={{color: theme.TEXT_WHITE, fontWeight: 'bold'}}
+                onClick={props.onFriendsPress}/>
+            </View>
             
-            <TouchableOpacity style={styles.friendsContainer}>
-                <Text style={styles.name}>Friends: {props.friendCount}</Text>
-
-            </TouchableOpacity>
         </View>
     )
 }
@@ -19,9 +22,11 @@ const ProfileHeader = (props) => {
 const styles = StyleSheet.create({
     profileHeader: {
         width: '100%',
-        height: 300,
-        justifyContent: 'space-around',
+        height: 200,
         alignItems: 'center',
+        justifyContent: 'flex-end',
+        //borderBottomWidth: 1,
+        //borderColor: theme.TEXT_LESS_WHITE,
 
     },
     name: {
@@ -37,11 +42,21 @@ const styles = StyleSheet.create({
         backgroundColor: 'green',
         borderRadius: 50,
     },
-    friendsContainer: {
+    friendsButton: {
         padding: 10,
-        borderRightWidth: 2,
-        borderLeftWidth: 2,
-        borderColor: '#32344c'
+        backgroundColor: theme.BUTTON_DARK_BLUE,
+        borderRadius: 20,
+    },
+    AddPost: {
+        height: 40,
+        width: '30%',
+        backgroundColor: theme.YELLOW,
+        borderRadius: 15,
+        marginTop: 15
+    },
+    AddPostText: {
+        fontWeight: 'bold',
+        color: theme.TEXT_WHITE
     }
 })
 
