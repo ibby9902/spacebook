b16356fa4ac9ffe4dda21081c5f2b073ed3f43cd
Ibby <ibby364@outlook.com>
2022-03-02 00:22:28 +0000

    Add send post button, states, basic validation

--- components/screens/AddNewPost.js
+++ components/screens/AddNewPost.js
@@ -3,18 +3,24 @@ import { View, StyleSheet } from 'react-native';
 import theme from '../../assets/theme'
 import CustomButton from '../common/CustomButton';
 import CustomInput from '../common/CustomInput';
-
+import AddPost from '../../functions/requests/AddPost';
 const AddNewPost = (props) => {
     const [postText, onChangePostText] = useState('');
+    const [postSent, setPostSent] = useState(false);
+    const [postError, setPostError] = useState(false);
     useEffect(() => {
         props.navigation.setOptions({title: "Add a new post!" })
+        
     })
 
-
+    const handleAddPost = () => {
+        if(postText !== '')
+            AddPost(props.route.params.user_id, postText, setPostSent, setPostError)
+    }
     return (
         <View style={styles.container}>
             <CustomInput multiline={true} setValue={onChangePostText} value={postText} placeholder={"What's happening?"} styles={styles.input}/>
-            <CustomButton text="Send Post" style={styles.button} textStyle={{color: theme.TEXT_WHITE}}/>
+            <CustomButton text="Send Post" style={styles.button} textStyle={{color: theme.TEXT_WHITE}} onClick={handleAddPost}/>
         </View>
     )
 }
