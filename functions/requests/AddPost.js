2c48ec8aeb56afa901b7c550853797ccaf77b9ee
Ibby <ibby364@outlook.com>
2022-03-02 00:23:05 +0000

    AddPost request

---
+++ functions/requests/AddPost.js
@@ -0,0 +1,26 @@
+import getToken from "../getToken";
+const AddPost = async(user_id, text, setPostSent, setPostError) => {
+    const token = await getToken();
+    const data = {text: text}
+    return fetch(`http://localhost:3333/api/1.0.0/user/${user_id}/post`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            "X-AUTHORIZATION": token
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(response.status === 201){
+            setPostSent(true);
+        }
+        else{
+            setPostError(true);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+export default AddPost;
