072382cdafce77daf60409084dc2c419e26c556e
Ibby <ibby364@outlook.com>
2022-03-03 01:55:12 +0000

    Added Search Stack

--- components/Main.js
+++ components/Main.js
@@ -3,7 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { ActivityIndicator, View} from 'react-native'
 import ProfileStack from './navigation/ProfileStack'
 import FriendsStack from './navigation/FriendsStack'
-import Search from './screens/Search'
+import SearchStack from './navigation/SearchStack';
 import getId from '../functions/getId';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import getToken from '../functions/getToken'
@@ -37,7 +37,7 @@ const Main = () => {
                         if(route.name === 'ProfileStack'){
                             iconName = 'person-circle-outline'
                             color = focused ? theme.TEXT_WHITE : theme.TEXT_GREY
-                        } else if(route.name === 'Search'){
+                        } else if(route.name === 'SearchStack'){
                             iconName = 'search-outline'
                             color = focused ? theme.TEXT_WHITE : theme.TEXT_GREY
                         } else if(route.name === 'Friends'){
@@ -51,7 +51,7 @@ const Main = () => {
                     },
                 })}>
                     <Tab.Screen name="ProfileStack" component={ProfileStack} options={{headerShown: false}} initialParams={{id: id}}/>
-                    <Tab.Screen name="Search" component={Search} options={{headerShown: false}}/>
+                    <Tab.Screen name="SearchStack" component={SearchStack} options={{headerShown: false}}/>
                     <Tab.Screen name="Friends" component={FriendsStack} options={{headerShown: false, unmountOnBlur: true}} initialParams={{id: id}}/>
                     <Tab.Screen name="Settings" component={Settings} options={{headerShown: false}}/>
                 </Tab.Navigator>
