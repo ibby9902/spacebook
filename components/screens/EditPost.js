c84272d9cfbc4f186acbe481d5ecd859c69efad1
Ibby <ibby364@outlook.com>
2022-03-03 04:27:21 +0000

    Created Edit Post Screen

---
+++ components/screens/EditPost.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect } from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+const EditPost = () => {
+    return (
+        <View>
+            <Text>Edit Post Screen</Text>
+        </View>
+    )
+}
+export default EditPost;
