500e0e5da86bb5b4c20b6a771cd2798ab2b39434
Ibrahim Hussein <ibby364@outlook.com>
2022-03-03 05:52:19 +0000

    Init Draft screen

---
+++ components/screens/Drafts.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import theme from '../../assets/theme'
+import CustomButton from '../common/CustomButton';
+import CustomInput from '../common/CustomInput';
+
+const Drafts = () => {
+    return (
+        <View>
+            <Text>DRAFTS</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Drafts;
