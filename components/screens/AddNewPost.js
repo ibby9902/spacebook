a8a541b85ccaf90658e98f4f4834a102bf76fa2b
Ibrahim Hussein <ibby364@outlook.com>
2022-03-03 05:53:51 +0000

    Added drafts button & handle function

--- components/screens/AddNewPost.js
+++ components/screens/AddNewPost.js
@@ -4,6 +4,7 @@ import theme from '../../assets/theme'
 import CustomButton from '../common/CustomButton';
 import CustomInput from '../common/CustomInput';
 import AddPost from '../../functions/requests/AddPost';
+import appendDraft from '../../functions/appendDraft';
 const AddNewPost = (props) => {
     const [postText, onChangePostText] = useState('');
     const [postSent, setPostSent] = useState(false);
@@ -17,10 +18,16 @@ const AddNewPost = (props) => {
         if(postText !== '')
             AddPost(props.route.params.user_id, postText, setPostSent, setPostError)
     }
+
+    const handleDraft = () => {
+        appendDraft(postText);
+    }
     return (
         <View style={styles.container}>
+            <CustomButton text="View Drafts" style={styles.viewDrafts} onClick={() => props.navigation.navigate("Drafts")}/>
             <CustomInput multiline={true} setValue={onChangePostText} value={postText} placeholder={"What's happening?"} styles={styles.input}/>
             <CustomButton text="Send Post" style={styles.button} textStyle={{color: theme.TEXT_WHITE}} onClick={handleAddPost}/>
+            <CustomButton text="Save Post as draft" style={styles.draft} textStyle={{color: theme.TEXT_WHITE}} onClick={handleDraft} />
         </View>
     )
 }
@@ -39,6 +46,16 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 30,
         backgroundColor: theme.YELLOW
+    },
+    draft: {
+        width: '100%',
+        height: 30,
+        backgroundColor: theme.YELLOW
+    },
+    viewDrafts: {
+        width: 80,
+        height: 30,
+        backgroundColor: 'red'
     }
 })
 
