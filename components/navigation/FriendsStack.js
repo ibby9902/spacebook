a613701e16933f486ceb7a1c3bd8867fb6a918b8
Ibby <ibby364@outlook.com>
2022-03-03 04:21:16 +0000

    Fix switching nav bug, added single post to stack

--- components/navigation/FriendsStack.js
+++ components/navigation/FriendsStack.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MyFriends from '../screens/MyFriends';
 import FriendRequests from '../screens/FriendRequests';
-import Profile from '../screens/Profile'
+import SinglePost from '../screens/SinglePost';
+import Profile from '../screens/Profile';
+
 const Stack = createNativeStackNavigator();
 
 const FriendsStack = ({route}) => {
@@ -12,6 +14,7 @@ const FriendsStack = ({route}) => {
             <Stack.Screen name="MyFriends" component={MyFriends} options={{headerShown: false}} initialParams={{id: route.params.id}}/>
             <Stack.Screen name="Friend Requests" component={FriendRequests} />
             <Stack.Screen name="FriendsProfile" component={Profile} options={{headerShown: false}} initialParams={{tabProfile: false}}/>
+            <Stack.Screen name="SinglePost" component={SinglePost} />
         </Stack.Navigator>
     )
 }
