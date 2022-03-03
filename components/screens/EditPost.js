af12c9a8860cc81e6ce2b04747d8810acb6d057c
Ibby <ibby364@outlook.com>
2022-03-03 04:42:22 +0000

    import edit post

--- components/screens/EditPost.js
+++ components/screens/EditPost.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import editPost from '../../functions/requests/editPost';
 const EditPost = () => {
+
     return (
         <View>
             <Text>Edit Post Screen</Text>
