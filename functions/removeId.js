767c2b9c9fd0f30988075a017f70972ca794a147
Ibby <ibby364@outlook.com>
2022-03-03 02:10:36 +0000

    Added remove id from async storage

---
+++ functions/removeId.js
@@ -0,0 +1,10 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const removeId = async () => {
+    try {
+        await AsyncStorage.removeItem("@id");
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+export default removeId;
