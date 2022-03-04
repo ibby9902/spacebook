d51367253f99229138e9f72c81e8ebdec434b8ad
Ibby <ibby364@outlook.com>
2022-03-04 08:43:39 +0000

    Delete post & set states

--- functions/requests/deletePost.js
+++ functions/requests/deletePost.js
@@ -1,5 +1,23 @@
 import getToken from "../getToken";
-const deletePost = () => {
-    console.log("post deleted")
+const deletePost = async (user_id, post_id, setPostDeleted, setPostDeleteError) => {
+    const token = await getToken();
+    return fetch(`http://localhost:3333/api/1.0.0/user/${user_id}/post/${post_id}`, {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json',
+            "X-AUTHORIZATION": token
+        },
+    })
+    .then((response) => {
+        if(response.status === 200){
+            setPostDeleted(true);
+        }
+        else{
+            setPostDeleteError(true);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 }
 export default deletePost;
