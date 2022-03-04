f8892bff95e8b08a1ac6ec2a5d9dd57dc356eabf
Ibby <ibby364@outlook.com>
2022-03-04 02:41:32 +0000

    Fixed styles, add account info button & navigtion functionality

--- components/screens/Settings.js
+++ components/screens/Settings.js
@@ -4,10 +4,15 @@ import CustomButton from '../common/CustomButton';
 import theme from '../../assets/theme';
 import logout from '../../functions/requests/logout';
 const Settings = (props) => {
-
+    
     return (
         <View style={styles.container}>
-            <CustomButton text="Logout" onClick={() => logout(props.route.params.navigation)} style={styles.logout}/>
+
+            <CustomButton text="Account Information" onClick={()=> props.navigation.navigate("AccountInfo")} style={styles.accountInfo}
+            textStyle={{fontWeight: 'bold', color: theme.TEXT_WHITE}}/>
+
+            <CustomButton text="Logout" onClick={() => logout(props.route.params.navigation)} style={styles.logout} textStyle={styles.logoutText}/>
+            
         </View>
     )
 }
@@ -16,12 +21,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: theme.DARK_GREY,
-        alignItems: 'center'
+        paddingHorizontal: 10,
+        paddingTop: 80,
+    },
+    accountInfo: {
+        width: '100%',
+        height: 50,
+        backgroundColor: theme.NAVBAR_BLUE
+    },
+    changePassword: {
+        width: '100%',
+        height: 50,
+        backgroundColor: theme.NAVBAR_BLUE,
+        marginTop: 10,
     },
     logout: {
-        width: '20%',
-        height: 30,
-        backgroundColor: theme.YELLOW
+        width: '100%',
+        height: 50,
+
+    },
+    logoutText: {
+        color: 'red',
+        fontWeight: 'bold'
     }
 })
 export default Settings;
