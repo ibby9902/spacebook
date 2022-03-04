c3c381d1bf3d9bb162ce3c32b62d9921f849b556
Ibby <ibby364@outlook.com>
2022-03-04 11:21:45 +0000

    Removed unnecessary params

--- components/common/ProfileHeader.js
+++ components/common/ProfileHeader.js
@@ -8,7 +8,7 @@ const ProfileHeader = (props) => {
     const renderProfilePhotoOrClickable = () => {
         if(props.isMyProfile) {
             return (
-                <TouchableOpacity style={styles.imageContainer} onPress={() => props.navigation.navigate("UploadImage", {pickImage: props.pickImage})}>
+                <TouchableOpacity style={styles.imageContainer} onPress={() => props.navigation.navigate("UploadImage")}>
                     <Image source={props.photo} style={{flex: 1}}/>
                 </TouchableOpacity>
             )
