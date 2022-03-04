35ff2d74dc6e19ba1129f220890fb4320f3af0f2
Ibby <ibby364@outlook.com>
2022-03-04 03:18:21 +0000

    Created get photo, convert to object url and setphoto

---
+++ functions/requests/getProfilePhoto.js
@@ -0,0 +1,24 @@
+import getToken from "../getToken";
+
+const getProfilePhoto = async (id, setPhoto) => {
+    const token =  await getToken();
+    return fetch(`http://localhost:3333/api/1.0.0/user/${id}/photo`,{
+            headers: {
+                "X-AUTHORIZATION": token
+            },
+            method: 'get'
+        })
+        .then((response) => {
+            if(response.status === 200)
+                return response.blob();
+        })
+        .then((img) => {
+           const obj = URL.createObjectURL(img);
+           setPhoto(obj)
+        })
+        .catch((error) => {
+            console.log(error)
+        })   
+}
+
+export default getProfilePhoto;
