988369366077d80386f24c9be5c3f754ee36ccdc
Ibby <ibby364@outlook.com>
2022-03-04 02:24:50 +0000

    Renamed screen to account info

--- components/navigation/SettingsStack.js
+++ components/navigation/SettingsStack.js
@@ -1,13 +1,13 @@
 import React from 'react';  
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Settings from '../screens/Settings';
-import UpdateInfo from '../screens/UpdateInfo'
+import AccountInfo from '../screens/AccountInfo'
 const Stack = createNativeStackNavigator();
 const SettingsStack = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Settings" component={Settings} options={{headerShown: false}}/>
-            <Stack.Screen name="UpdateInfo" component={UpdateInfo} />
+            <Stack.Screen name="AccountInfo" component={AccountInfo} />
         </Stack.Navigator>
     )
 }
