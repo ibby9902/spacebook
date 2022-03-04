2d9553ca634e2b73921b047149c6d8ad13521ebd
Ibby <ibby364@outlook.com>
2022-03-04 11:22:27 +0000

    Added UploadImage to stack & removed comments

--- components/navigation/ProfileStack.js
+++ components/navigation/ProfileStack.js
@@ -5,21 +5,20 @@ import SinglePost from '../screens/SinglePost';
 import AddNewPost from '../screens/AddNewPost';
 import EditPost from '../screens/EditPost';
 import Drafts from '../screens/Drafts';
+import UploadImage from '../screens/UploadImage';
 const Stack = createNativeStackNavigator();
 
 const ProfileStack = ({route}) => {
     return (
         <Stack.Navigator initialRouteName='Profile'>
                         <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}} initialParams={{id: route.params.id, tabProfile: true}}/>
-                        {/*a whole screen with a list of friends? */}
                         <Stack.Screen name="UsersFriends" component={UsersFriends} initialParams={{id: route.params.id, tabProfile: false}}/>
                         <Stack.Screen name="SinglePost" component={SinglePost} />
                         <Stack.Screen name="FriendsProfile" component={Profile} options={{headerShown: false}}/>
                         <Stack.Screen name="AddNewPost" component={AddNewPost}/>
                         <Stack.Screen name="EditPost" component={EditPost}/>
                         <Stack.Screen name="Drafts" component={Drafts}/>
-                        {/* <Stack.Screen name="SinglePost" component={Signup}/> A whole screen for a single post??*/}
-                        {/* Profile stack screen here? So we can click friends on the friends list to show their profile? */} 
+                        <Stack.Screen name="UploadImage" component={UploadImage}/>
         </Stack.Navigator>
     )
 }
