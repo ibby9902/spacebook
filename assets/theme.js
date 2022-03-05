f3fb2cd638068104e460de9dca5dec62be56f97b
Ibby <ibby364@outlook.com>
2022-03-05 10:18:05 +0000

    Fixed all ESLint issues

--- assets/theme.js
+++ assets/theme.js
@@ -1,16 +1,16 @@
 const theme = {
-    VERY_DARK_GREY: '#f7d04d',
-    DARK_GREY: '#13161b', // background
-    WHITE: 'white',
-    LIGHT_GRAY: '#1e2227',
-    YELLOW: '#f7d04d',
-    NAVBAR_BLUE: '#191c24',
-    ICON_INACTIVE: '#7f8081',
-    TEXT_WHITE: '#f5f5f5',
-    TEXT_LESS_WHITE: '#cacbcc',
-    TEXT_GREY: '#9fa1a2',
-    BUTTON_DARK_BLUE: '#1f2531',
-    GREY_BLUE: '#25292f'
-}
+  VERY_DARK_GREY: '#f7d04d',
+  DARK_GREY: '#13161b',
+  WHITE: 'white',
+  LIGHT_GRAY: '#1e2227',
+  YELLOW: '#f7d04d',
+  NAVBAR_BLUE: '#191c24',
+  ICON_INACTIVE: '#7f8081',
+  TEXT_WHITE: '#f5f5f5',
+  TEXT_LESS_WHITE: '#cacbcc',
+  TEXT_GREY: '#9fa1a2',
+  BUTTON_DARK_BLUE: '#1f2531',
+  GREY_BLUE: '#25292f',
+};
 
 export default theme;
