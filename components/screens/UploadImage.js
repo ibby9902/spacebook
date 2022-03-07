0ebd1730fcd4c097d0211393a86536fbaadee283
Ibby <ibby364@outlook.com>
2022-03-07 12:45:14 +0000

    Separate choose image & upload image, add image preview & status text

--- components/screens/UploadImage.js
+++ components/screens/UploadImage.js
@@ -8,6 +8,9 @@ import uploadImage from '../../functions/requests/uploadImage';
 const UploadImage = (props) => {
   const [image, setUploadImage] = useState(null);
   const [imageUploaded, setImageUploaded] = useState(false);
+  const [previewImage, setPreviewImage] = useState(null);
+  const [imageUploadSuccess, setImageUploadedSuccess] = useState(false);
+  const [imageUploadError, setImageUploadedError] = useState(false);
 
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -19,13 +22,35 @@ const UploadImage = (props) => {
 
     if (!result.cancelled) {
       setUploadImage(result.uri);
-      uploadImage(result.uri, setImageUploaded);
+      setPreviewImage(result.uri);
+    }
+  };
+
+  const handleUpload = () => {
+    uploadImage(image, setImageUploaded, setImageUploadedSuccess, setImageUploadedError);
+  };
+
+  const successText = () => {
+    if (imageUploadSuccess) {
+      return <Text style={styles.statusText}>Image Uploaded!</Text>;
+    }
+  };
+
+  const errorText = () => {
+    if (imageUploadError) {
+      return <Text style={styles.statusText}>Error uploading image</Text>;
     }
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={previewImage} style={{ flex: 1 }} />
+      </View>
       <CustomButton text="Choose image" onClick={pickImage} style={styles.pickImageButton} textStyle={styles.pickImageButtonText} />
+      <CustomButton text="Upload Image" onClick={handleUpload} style={styles.pickImageButton} textStyle={styles.pickImageButtonText} />
+      {successText()}
+      {errorText()}
     </View>
   );
 };
@@ -33,6 +58,8 @@ const UploadImage = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: theme.DARK_GREY,
     padding: 10,
   },
@@ -40,8 +67,18 @@ const styles = StyleSheet.create({
     width: 100,
     height: 60,
     backgroundColor: theme.YELLOW,
+    marginTop: 5,
   },
   pickImageButtonText: {
+    color: theme.TEXT_WHITE,
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+  },
+  statusText: {
     color: theme.TEXT_WHITE,
     fontWeight: 'bold',
   },
