096cd9a37f5613b556c8dec2fe1c8274c0c68ea4
Ibby <ibby364@outlook.com>
2022-03-08 04:46:41 +0000

    Created Drafts screen

--- components/screens/Drafts.js
+++ components/screens/Drafts.js
@@ -1,19 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import theme from '../../assets/theme'
-import CustomButton from '../common/CustomButton';
-import CustomInput from '../common/CustomInput';
+import { View, StyleSheet, ScrollView, FlatList } from 'react-native';
+import theme from '../../assets/theme';
+import getDrafts from '../../functions/getDrafts';
+import Draft from '../common/Draft';
 
 const Drafts = () => {
-    return (
-        <View>
-            <Text>DRAFTS</Text>
-        </View>
-    )
-}
+  const [drafts, setDrafts] = useState([]);
+  const [draftDeleted, setDraftDeleted] = useState(false);
+  useEffect(() => {
+    getDrafts().then((res) => setDrafts(res));
+  }, []);
+
+  useEffect(() => {
+    getDrafts().then((res) => setDrafts(res));
+    setDraftDeleted(false);
+  }, [draftDeleted]);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={drafts}
+        renderItem={({ item, index }) => <Draft text={item.post} index={index} setDraftDeleted={setDraftDeleted} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: theme.DARK_GREY,
+    padding: 10,
+  },
+});
 
 export default Drafts;
